e1c0e4c25a65485df6e8d3af5710f3016678a34f
anastasiia semenova <sanasteisha31@gmail.com>
2025-04-03 18:51:28 -0400

    "types fixed"

--- lib/getArtworks.ts
+++ lib/getArtworks.ts
@@ -6,13 +6,11 @@ export default async function getArtworks(): Promise<Artwork[]> {
     const data = await fetch(`https://api.harvardartmuseums.org/object?apikey=${key}&size=10`)
 
     //throw an error here
-    if (!data.ok) {
-        throw new Error("Could not fetch artwork data");
-    }
 
-    const json= await data.json();
 
-    return json.records.map((item) => ({
+    const actual = await data.json();
+
+    return actual.records.map((item:object) => ({
         title: item.title || "Untitled",
         culture: item.culture || "Unknown",
         dated: item.dated || "Unknown",
